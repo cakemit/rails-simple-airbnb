8e0ee0ff51b5f331a4339ce27952c164eb1d7370
Claudia Tanaka <catanaka@hotmail.com>
2020-11-29 21:07:09 -0300

    model created with DB and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,16 @@
+Faker::Config.locale = 'pt-BR'
 
-10.times do
-  
+puts "Cleaning all databases"
+Flat.destroy_all
+
+puts "Creating 10 flats..."
+for i in (1..10) do
+  flat = Flat.create(
+    name: [Faker::Book.title, Faker::Book.author].join(' - '),
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph(sentence_count: 10),
+    price_per_night: rand(100..1000),
+    number_of_guests: rand(1..5))
+  puts "Created #{i}. #{flat.name}"
 end
+puts "Finished"
